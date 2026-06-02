34c798e22e9937fcdb5d6b3bbe3b17985aa532fb
黄佳兴 <519207704@qq.com>
2026-06-02 17:10:51 +0800

    配置HSE LSE

--- Core/Src/gpio.c
+++ Core/Src/gpio.c
@@ -33,6 +33,10 @@
 /* USER CODE END 1 */
 
 /** Configure pins
+     PC14-OSC32_IN (OSC32_IN)   ------> RCC_OSC32_IN
+     PC15-OSC32_OUT (OSC32_OUT)   ------> RCC_OSC32_OUT
+     PH0-OSC_IN (PH0)   ------> RCC_OSC_IN
+     PH1-OSC_OUT (PH1)   ------> RCC_OSC_OUT
 */
 void MX_GPIO_Init(void)
 {
@@ -40,6 +44,8 @@ void MX_GPIO_Init(void)
   GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
