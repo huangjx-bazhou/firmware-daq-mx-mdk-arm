c9b8a48c507e6ea418397c3145ea94f2d2008fa8
黄佳兴 <519207704@qq.com>
2026-07-10 18:15:16 +0800

    GY-95T函数

--- Logic/Inc/gy95t.h
+++ Logic/Inc/gy95t.h
@@ -25,10 +25,28 @@
 extern "C" {
 #endif
 
+#include "ringbuffer.h"
 #include "usart.h"
 
-// usart2接收缓存
-extern volatile uint8_t g_usart2_rx_byte;
+#define GY95T_DATA_SIZE 18U
+
+/** gy95t usart2接收缓存 */
+extern volatile uint8_t g_gy95t_usart2_rx_byte;
+
+/** gy95t 环形缓冲区 */
+extern RingBuffer g_gy95t_rb;
+
+/** gy95t 数据缓存 */
+extern uint8_t g_gy95t_data[GY95T_DATA_SIZE];
+
+/*
+ * @brief GY95T初始化
+ *
+ * @author  黄佳兴
+ * @version 0.1
+ * @date    2026-07-10
+ */
+void GY95T_Init(void);
 
 /*
  * @brief GY95T开始连续输出
@@ -47,6 +65,15 @@ void GY95T_Start(void);
  * @date    2026-07-09
  */
 void GY95T_Stop(void);
+
+/*
+ * @brief GY95T处理一字节数据
+ *
+ * @author  黄佳兴
+ * @version 0.1
+ * @date    2026-07-10
+ */
+void GY95T_ProcessData(uint8_t data);
 
 #ifdef __cplusplus
 }
--- Logic/Src/gy95t.c
+++ Logic/Src/gy95t.c
@@ -3,7 +3,7 @@
   ******************************************************************************
   * @file    gy95t.c
   * @brief   This file provides code for the acquisition of gy95t.
-  *          
+  *
   ******************************************************************************
   * @attention
   *
@@ -20,17 +20,40 @@
 /* Includes ------------------------------------------------------------------*/
 #include "gy95t.h"
 
-// 连续输出命令
-const uint8_t gy95t_start_cmd[] = { 0x00, 0x06, 0x03, 0x00, 0x09 };
+/** 连续输出命令 */
+static const uint8_t gy95t_start_cmd[] = { 0x00, 0x06, 0x03, 0x00, 0x09 };
 
-// 查询输出命令
-const uint8_t gy95t_stop_cmd[] = { 0x00, 0x06, 0x03, 0x01, 0x0A };
+/** 查询输出命令 */
+static const uint8_t gy95t_stop_cmd[] = { 0x00, 0x06, 0x03, 0x01, 0x0A };
 
-// 超时时间
-const uint32_t gy95t_timeout = 100U;
+/** 超时时间100ms */
+static const uint32_t gy95t_timeout = 100U;
 
-// usart2接收缓存
-volatile uint8_t g_usart2_rx_byte;
+/** gy95t usart2接收缓存 */
+volatile uint8_t g_gy95t_usart2_rx_byte;
+
+/** gy95t 环形缓冲区 */
+RingBuffer g_gy95t_rb;
+
+/** gy95t 数据缓存 */
+uint8_t g_gy95t_data[GY95T_DATA_SIZE];
+
+#define RECEIVED_NONE 0U
+#define RECEIVED_A4   1U
+#define RECEIVED_03   2U
+#define RECEIVED_08   3U
+#define RECEIVED_1B   4U
+
+/** 状态 */
+static uint8_t state = RECEIVED_NONE;
+
+/** 收到的数据长度 */
+static uint8_t received = 0U;
+
+void GY95T_Init(void)
+{
+  RB_Init(&g_gy95t_rb);
+}
 
 void GY95T_Start(void)
 {
@@ -40,4 +63,40 @@ void GY95T_Start(void)
 void GY95T_Stop(void)
 {
   HAL_UART_Transmit(&huart2, gy95t_stop_cmd, sizeof(gy95t_stop_cmd), gy95t_timeout);
+}
+
+void GY95T_ProcessData(uint8_t data)
+{
+  switch (state)
+  {
+    case RECEIVED_NONE:
+      state = 0xA4 == data ? RECEIVED_A4 : RECEIVED_NONE;
+      break;
+    case RECEIVED_A4:
+      state = 0x03 == data ? RECEIVED_03 : RECEIVED_NONE;
+      break;
+    case RECEIVED_03:
+      state = 0x08 == data ? RECEIVED_08 : RECEIVED_NONE;
+      break;
+    case RECEIVED_08:
+      state = 0x1B == data ? RECEIVED_1B : RECEIVED_NONE;
+      break;
+    case RECEIVED_1B:
+      if (received >= 28U)
+      {
+        received = 0U;
+        state = RECEIVED_NONE;
+      }
+      else if (received >= GY95T_DATA_SIZE)
+      {
+        received++;
+      }
+      else
+      {
+        g_gy95t_data[received++] = data;
+      }
+      break;
+    default:
+      break;
+  }
 }
