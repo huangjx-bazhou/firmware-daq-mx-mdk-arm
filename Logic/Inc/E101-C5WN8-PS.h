178bce8b5b0aef3bdafea1d033c973a63d32ae0a
黄佳兴 <519207704@qq.com>
2026-07-13 11:15:38 +0800

    WIFI模块代码优化

---
+++ Logic/Inc/E101-C5WN8-PS.h
@@ -0,0 +1,113 @@
+﻿/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    E101-C5WN8-PS.h
+  * @brief   This file contains all the function prototypes for
+  *          the E101-C5WN8-PS.c file
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __E101_C5WN8_PS_H__
+#define __E101_C5WN8_PS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "main.h"
+#include <stdbool.h>
+#include "ringbuffer.h"
+
+/** LED数量，两个TLC59116，每个连接8个LED */
+#define LED_COUNT 16U
+
+/** 每个LED的通道数量（每个LED有2个波长的通道） */
+#define CHANNEL_COUNT_PER_LED 2U
+
+/** 接收器数量，两个ADS1299，每个连接8个接收器 */
+#define RECVIVER_COUNT 16U
+
+/** 命令头 */
+#define CMD_HEADER 0x846F
+
+/** wifi usart3接收缓存 */
+extern volatile uint8_t g_wifi_usart3_rx_byte;
+
+/** wifi 环形缓冲区 */
+extern RingBuffer g_wifi_rb;
+
+/* 以下是解析出来的命令参数 ------------------------------------------------------------------*/
+
+/**  时间戳, 上位机应该传0，下位机每隔100us增加1 */
+extern volatile uint32_t g_timestamp;
+/**  光源的亮度 */
+extern uint8_t g_led_brightness[LED_COUNT];
+/**  通道标志 */
+extern uint8_t g_channel_mask[LED_COUNT * RECVIVER_COUNT / 8];
+
+/**  开关机标志是否改变 */
+extern bool g_power_flag_changed;
+/**  开关机标志 */
+extern bool g_power_flag;
+
+/**  离线存储标志是否改变 */
+extern bool g_storage_flag_changed;
+/**  离线存储标志 */
+extern bool g_storage_flag;
+
+/**  采样率是否改变 */
+extern bool g_sample_rate_changed;
+/**  采样率 */
+extern uint8_t g_sample_rate;
+
+/**  开始采样标志是否改变 */
+extern bool g_start_flag_changed;
+/**  开始采样标志 */
+extern bool g_start_flag;
+
+/*
+ * @brief 初始化WIFI模块
+ *
+ * @author  黄佳兴
+ * @version 0.1
+ * @date    2026-07-13
+*/
+void WIFI_Init(void);
+
+/*
+ * @brief 组装命令
+ *
+ * @param uint8_t byte 要处理的字节
+ *
+ * @author  黄佳兴
+ * @version 0.1
+ * @date    2026-07-10
+*/
+void WIFI_AssembleCommand(uint8_t byte);
+
+/*
+ * @brief 解析命令
+ *
+ * @param uint8_t* cmd 要解析的命令指针
+ *
+ * @author  黄佳兴
+ * @version 0.1
+ * @date    2026-06-17
+*/
+void WIFI_ParseCommand(uint8_t* cmd);
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__E101_C5WN8_PS_H__ */
---
+++ Logic/Src/E101-C5WN8-PS.c
@@ -0,0 +1,144 @@
+﻿/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    E101-C5WN8-PS.c
+  * @brief   This file provides code for the E101-C5WN8-PS.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "E101-C5WN8-PS.h"
+#include <string.h>
+
+#define CMD_PACKET_SIZE 59U
+#define CMD_ENABLE_FLAG 0x55
+#define CMD_DISABLE_FLAG 0xAA
+#define RECEIVED_NONE 0U
+#define RECEIVED_84   1U
+#define RECEIVED_6F   2U
+
+volatile uint8_t g_wifi_usart3_rx_byte = 0U;
+
+RingBuffer g_wifi_rb;
+
+/** wifi 命令 */
+static uint8_t wifi_cmd[CMD_PACKET_SIZE];
+
+/** 状态 */
+static uint8_t wifi_received_state = RECEIVED_NONE;
+
+/** 收到的数据长度 */
+static uint8_t wifi_received = 0U;
+
+volatile uint32_t g_timestamp = 0U;
+uint8_t g_led_brightness[LED_COUNT];
+uint8_t g_channel_mask[LED_COUNT * RECVIVER_COUNT / 8];
+bool g_power_flag_changed = false;
+bool g_power_flag = true;
+bool g_storage_flag_changed = false;
+bool g_storage_flag = false;
+bool g_sample_rate_changed = false;
+uint8_t g_sample_rate = 50U;
+bool g_start_flag_changed = false;
+bool g_start_flag = false;
+
+void WIFI_Init(void)
+{
+  /* 初始化环形缓冲区 */
+  RB_Init(&g_wifi_rb);
+
+  /* 初始化LED亮度 */
+  memset(g_led_brightness, 0xFF, LED_COUNT);
+
+  /* TODO: 初始化通道掩码 */
+}
+
+void WIFI_AssembleCommand(uint8_t byte)
+{
+  switch (wifi_received_state)
+  {
+    case RECEIVED_NONE:
+      if (0x84 == byte)
+      {
+        wifi_cmd[wifi_received++] = byte;
+        wifi_received_state = RECEIVED_84;
+      }
+      break;
+    case RECEIVED_84:
+      if (0x6F == byte)
+      {
+        wifi_cmd[wifi_received++] = byte;
+        wifi_received_state = RECEIVED_6F;
+      }
+      else
+      {
+        wifi_received = 0;
+        wifi_received_state = RECEIVED_NONE;
+      }
+      break;
+    case RECEIVED_6F:
+      wifi_cmd[wifi_received++] = byte;
+      if (wifi_received >= CMD_PACKET_SIZE)
+      {
+        WIFI_ParseCommand(wifi_cmd);
+        wifi_received_state = RECEIVED_NONE;
+      }
+      break;
+  }
+}
+
+void WIFI_ParseCommand(uint8_t* cmd)
+{
+  // 不处理第3字节，因为它是0x0B，不感兴趣
+
+  // TODO: 验证时间戳是否解析正确
+  g_timestamp = (uint32_t)cmd[3];
+
+  // LED亮度解析
+  for (uint8_t i = 0; i < LED_COUNT; i++)
+  {
+    g_led_brightness[i] = cmd[7 + i];
+  }
+
+  // 通道掩码复制
+  memcpy(g_channel_mask, cmd + 23, LED_COUNT * RECVIVER_COUNT / 8);
+
+  /* 开关机标志解析 */
+  if (cmd[55] != g_power_flag)
+  {
+    g_power_flag = cmd[55];
+    g_power_flag_changed = true;
+  }
+
+  /* 离线存储标志解析 */
+  if (cmd[56] != g_storage_flag)
+  {
+    g_storage_flag = cmd[56];
+    g_storage_flag_changed = true;
+  }
+
+  /* 采样率解析 */
+  if (cmd[57] != g_sample_rate)
+  {
+    g_sample_rate = cmd[57];
+    g_sample_rate_changed = true;
+  }
+
+  /* 开始采样标志解析 */
+  if (cmd[58] != g_start_flag)
+  {
+    g_start_flag = cmd[58];
+    g_start_flag_changed = true;
+  }
+}
